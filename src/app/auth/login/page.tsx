0dcc2e826bc6759b9b095240fb7f2a2fc687e416
Allan <allan_hc@outlook.com>
2025-07-23 16:54:08 -0600

    Primer commit

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@radix-ui/themes";
+import { BookmarkIcon } from "@radix-ui/react-icons";
+
+
+export default function LoginPage() {
+    return (
+        <div>
+            LoginPage
+            <Button>
+                <BookmarkIcon /> Bookmark
+            </Button>
+        </div>
+    )
+}
